ff3b9b574d0b89e3dbee80f79a35d48128d253d3
abdo <abdo@gmail.com>
2020-01-05 14:05:48 +0100

    Add Css and Javascript from Template

---
+++ functions.php
@@ -0,0 +1,28 @@
+
+<?php 
+// Add Css enqueue
+
+<link rel="stylesheet" href="fonts/beyond_the_mountains-webfont.css" type="text/css"/>
+
+<!-- Stylesheets -->
+<link href="plugin-frameworks/bootstrap.min.css" rel="stylesheet">
+<link href="plugin-frameworks/swiper.css" rel="stylesheet">
+<link href="fonts/ionicons.css" rel="stylesheet">
+<link href="common/styles.css" rel="stylesheet">
+
+
+function load_css(){
+
+wp_register_style( 'font', get_template_directory_uri(  ).'/'
+a-body, $deps:array, $ver:string|boolean|null, $media:string )
+
+}
+
+
+
+
+
+
+
+
+?>
